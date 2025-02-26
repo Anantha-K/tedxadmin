1024eda0737c4a9348448bdb2983bb8583be601f
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-02-26 17:42:20 +0530

    tshirts

--- src/Pages/Tshirt.jsx
+++ src/Pages/Tshirt.jsx
@@ -33,11 +33,15 @@ const Tshirt = () => {
   return (
     <div style={{ width: "100vw", minHeight: "100vh", backgroundColor: "black" }}>
       <h1 style={{ color: "red", textAlign: "center" }}>Tshirt Sizes</h1>
-      {Object.entries(registrations.sizes).map(([key, value]) => (
-        <div key={key}>
-          {key}: {value}
-        </div>
-      ))}
+      <div style={{width:"100vw",marginTop:"10vh",display:"flex",justifyContent:"center",alignItems:"center",flexWrap:"wrap", gap:"50px", flexDirection:window.innerHeight>window.innerWidth?"column":"row"}}>
+        {Object.entries(registrations.sizes).map(([key, value]) => (
+          <div key={key} style={{width:window.innerHeight>window.innerWidth?"80vw":"20vw",display:"flex",justifyContent:"center",alignItems:"center",height:"30vh",backgroundColor:"#202020",borderRadius:"15px",flexDirection:"column",gap:"50px"}}>
+            <h1>{key}</h1>
+            <h2>{value}</h2>
+          </div>
+        ))}
+      </div>
+      
     </div>
   );
 }
