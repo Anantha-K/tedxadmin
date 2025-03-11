78dbae79e0863880dccb77cc7c0bb30961621964
Anantha Krishnan <jananthak@gmail.com>
2025-03-12 02:32:00 +0530

    fix

--- src/Pages/QrScanner.jsx
+++ src/Pages/QrScanner.jsx
@@ -204,8 +204,8 @@ const Scan = () => {
             <div style={{ fontWeight: "bold", color: "#6c757d" }}>Watch Party:</div>
             <div  style={{color: "#6c757d" }}>{participant.isWatchParty}</div>
             
-            {/* <div style={{ fontWeight: "bold", color: "#6c757d" }}>T-shirt Size:</div>
-            <div  style={{color: "#6c757d" }}>{participant.tshirtSize}</div> */}
+            <div style={{ fontWeight: "bold", color: "#6c757d" }}>T-shirt Size:</div>
+            <div  style={{color: "#6c757d" }}>{participant.tshirtSize}</div> 
             
             <div style={{ fontWeight: "bold", color: "#6c757d" }}>Present:</div>
             <div style={{ 
